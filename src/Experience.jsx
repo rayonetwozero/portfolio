7fb6d8dbe561c4b98334a5227a865a0284469545
雷政諭 <ray120@leizhengyudeMacBook-Pro.local>
2025-06-11 05:24:51 +0800

    update portfolio

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,16 +1,65 @@
-import { Text,Html,Float,OrbitControls, Environment,ContactShadows, Grid,PerspectiveCamera,useGLTF,useProgress } from '@react-three/drei'
-import { EffectComposer, ToneMapping,Noise } from '@react-three/postprocessing'
+import { Text,Html,Float,OrbitControls, Environment,ContactShadows, Grid,PerspectiveCamera,useGLTF,Outlines } from '@react-three/drei'
+import { EffectComposer, ToneMapping,Noise, Bloom } from '@react-three/postprocessing'
 import { ToneMappingMode,BlendFunction } from 'postprocessing'
-
+import * as THREE from 'three'
+import React, { useEffect } from 'react'
 
 
 export default function Experience()
 {
     
     const desk = useGLTF('/3d.glb')
-    
-    
+    const lamp = useGLTF('/lamp.glb')
+    const red_chair = useGLTF('/red-chair.glb')
+    const motocycle = useGLTF('/motocycle.glb')
 
+    // lamp-light
+    useEffect(() => {
+        if (lamp && lamp.scene) {
+            const lampLightMesh = lamp.scene.getObjectByName('lamp-light')
+            if (lampLightMesh && lampLightMesh.isMesh) {
+                lampLightMesh.material = new THREE.MeshStandardMaterial({
+                    color: 0xFFD306,
+                    emissive: 0xFFD306,
+                    emissiveIntensity: 10,
+                    metalness: 0.1,
+                    roughness: 0.3,
+                })
+            }
+        }
+    }, [lamp])
+
+    // moto-front-light
+    useEffect(() => {
+        if (motocycle && motocycle.scene) {
+            const motocycleLightMesh = motocycle.scene.getObjectByName('light-front')
+            if (motocycleLightMesh && motocycleLightMesh.isMesh) {
+                motocycleLightMesh.material = new THREE.MeshStandardMaterial({
+                    color: 0xFFD306,
+                    emissive: 0xFFD306,
+                    emissiveIntensity: 10,
+                    metalness: 0.1,
+                    roughness: 0.3,
+                })
+            }
+        }
+    }, [motocycle])
+
+    // moto-back-light
+    useEffect(() => {
+        if (motocycle && motocycle.scene) {
+            const motocycleLightMesh = motocycle.scene.getObjectByName('light-back')
+            if (motocycleLightMesh && motocycleLightMesh.isMesh) {
+                motocycleLightMesh.material = new THREE.MeshStandardMaterial({
+                    color: 0xFF0000,
+                    emissive: 0xFF0000,
+                    emissiveIntensity: 10,
+                    metalness: 0.1,
+                    roughness: 0.3,
+                })
+            }
+        }
+    }, [motocycle])
 
     return (
     <>
@@ -34,7 +83,7 @@ export default function Experience()
             target={[0, 1, 0]} 
             autoRotate
             autoRotateSpeed={0.1}
-            minAzimuthAngle={-Math.PI / 4}
+            minAzimuthAngle={-Math.PI / 3}
             maxAzimuthAngle={Math.PI / 6}
             minPolarAngle={Math.PI /3}
             maxPolarAngle={Math.PI / 2}
@@ -52,12 +101,17 @@ export default function Experience()
             position={ [ 0, 1.145, -0.38 ] }
             rotation-x={ 0 }
             >
-            <a href="https://ray120.gitbook.io/works" target="_blank" rel="noopener noreferrer">
+            <a href="https://ray120.gitbook.io/works" >
                 <img src="/cv-zh.jpg" alt="cv" style={{ width: '1920px', borderRadius: '4px', cursor: 'pointer' }} />
             </a>
             </Html>
         </primitive>
 
+        <primitive object={ lamp.scene }  ></primitive>
+        <primitive object={ red_chair.scene }  >
+            
+        </primitive>
+        <primitive object={ motocycle.scene }  ></primitive>
         
         <Float 
             floatIntensity={ 1 } 
@@ -110,7 +164,14 @@ export default function Experience()
                 opacity={ 0.4 }
                 offset={ [ 0.02, 0.02 ] }
             />
-        
+            <Bloom
+            intensity={0.5}      // 強度，可調整
+            luminanceThreshold={1.3} // 亮度門檻，低一點會讓更多東西發光
+            luminanceSmoothing={1} // 柔和度
+            mipmapBlur={true}
+            />
+
+
             <ToneMapping mode={ ToneMappingMode.ACES_FILMIC} /> {/* ToneMapping effect must put in back. */}
         </EffectComposer>
         
